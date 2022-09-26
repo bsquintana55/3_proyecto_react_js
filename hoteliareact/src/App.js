d19ff6497bbc696f89ebd2fff49712d8900a6da4
GabrielaMoron <104436274+GabrielaMoron@users.noreply.github.com>
2022-09-26 03:32:29 -0500

    componente registro

---
+++ hoteliareact/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import Registro from './componentes/Registro'
+function App() {
+  return (
+    <div className="App">
+     
+   
+     <Registro />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ hoteliareact/src/componentes/Registro.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Registro(){
+   return(
+  
+    <section className='bloque'>
+           <div className='logo'>
+            <img src={require('../img/Hotelia horizontal blanco.svg')} alt=''/>
+            <div className='txt'>
+            <h4 className='compt'>Eres más que  bienvenido</h4>
+            <h4 className='cel'>Un lugar en tu corazón</h4>
+        </div>
+        </div>
+      </section>
+    
+   );
+   
+} 
+export default Registro;
