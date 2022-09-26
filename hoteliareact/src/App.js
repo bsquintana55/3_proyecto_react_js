fa4172455263ad82023fd9a96dbf60115841ae17
GabrielaMoron <104436274+GabrielaMoron@users.noreply.github.com>
2022-09-26 04:16:44 -0500

    componente Registro

---
+++ hoteliareact/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Registro from './componentes/Registro'
+function App() {
+  return (
+    <div className="App">
+     
+   
+     <Registro />
+    </div>
+
+  );
+}
+
+export default App;
