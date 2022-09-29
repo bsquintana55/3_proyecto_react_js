4f06580a86722234fc0e6dbf34ee61c5bd83bbcc
bsquintana55 <bsquintana55@misena.edu.co>
2022-09-28 21:53:14 -0500

    favicon

--- reacthotelia/src/index.js
+++ reacthotelia/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+
+import Favicon from 'react-favicon'
+
+
 import './index.css';
-
-
-
-
-
 /*
 import Login from './components/login/login';
  <Login/>
@@ -30,8 +30,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
   <React.StrictMode>
- 
+ <Favicon url='http://oflisback.github.io/react-favicon/img/github.ico' />
  <Navbar/>
   <Banner/>
     <Info/>
