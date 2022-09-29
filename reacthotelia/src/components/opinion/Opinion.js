a4500868428664ede79aeecff78d9f0453042451
bsquintana55 <bsquintana55@misena.edu.co>
2022-09-28 21:34:31 -0500

    componentes arreglados

--- reacthotelia/src/components/opinion/Opinion.js
+++ reacthotelia/src/components/opinion/Opinion.js
@@ -1,4 +1,10 @@
 import './opi.css';
+import ReactDOM from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faCheckSquare, faCoffee)
 
 function Opinion() { 
     return(
